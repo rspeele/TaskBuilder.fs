47c12ec53f64bebe3c7004c434a58519d695be64
Robert Peele <robert.s.peele@gmail.com>
2016-10-20 23:37:52 -0400

    Add a simple file read/write benchmark to compare against C# async.

---
+++ BenchmarkCS/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BenchmarkCS
+{
+    static class Program
+    {
+        private const int BufferSize = 128;
+
+        private static async Task WriteFile(string path)
+        {
+            var junk = new byte[BufferSize];
+            using (var file = File.Create(path))
+            {
+                for (var i = 1; i <= 10000; i++)
+                {
+                    await file.WriteAsync(junk, 0, junk.Length);
+                }
+            }
+        }
+
+        private static async Task ReadFile(string path)
+        {
+            var buffer = new byte[BufferSize];
+            using (var file = File.OpenRead(path))
+            {
+                var reading = true;
+                while (reading)
+                {
+                    var countRead = await file.ReadAsync(buffer, 0, buffer.Length);
+                    reading = countRead > 0;
+                }
+            }
+        }
+
+        private static async Task Bench()
+        {
+            const string tmp = "tmp";
+            var sw = new Stopwatch();
+            sw.Start();
+            for (var i = 1; i <= 10; i++)
+            {
+                await WriteFile(tmp);
+                await ReadFile(tmp);
+            }
+            sw.Stop();
+            Console.WriteLine($"Completed in {sw.ElapsedMilliseconds} ms");
+            File.Delete(tmp);
+        }
+
+        static void Main(string[] args)
+            => Bench().Wait();
+    }
+}
---
+++ BenchmarkFS/Program.fs
@@ -0,0 +1,42 @@
+﻿open FSharp.Control.Tasks
+open System.Diagnostics
+open System.Threading.Tasks
+open System.IO
+
+let bufferSize = 128
+
+let writeFile path =
+    task {
+        let junk = Array.zeroCreate bufferSize
+        use file = File.Create(path)
+        for i = 1 to 10000 do
+            do! file.WriteAsync(junk, 0, junk.Length)
+    }
+
+let readFile path =
+    task {
+        let buffer = Array.zeroCreate bufferSize
+        use file = File.OpenRead(path)
+        let mutable reading = true
+        while reading do
+            let! countRead = file.ReadAsync(buffer, 0, buffer.Length)
+            reading <- countRead > 0
+    }
+
+let bench() =
+    let tmp = "tmp"
+    task {
+        let sw = Stopwatch()
+        sw.Start()
+        for i = 1 to 10 do
+            do! writeFile tmp
+            do! readFile tmp
+        sw.Stop()
+        printfn "Completed in %d ms" sw.ElapsedMilliseconds
+        File.Delete(tmp)
+    }
+
+[<EntryPoint>]
+let main argv = 
+    bench().Wait()
+    0 // return an integer exit code
